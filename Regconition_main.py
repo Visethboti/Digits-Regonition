042f0865d190742c4d40c7a19e7c60aadc87c3f1
Visethboti <visethboti@gmail.com>
2020-06-10 16:35:55 +0700

    Working with 2 digits, switch to use convolutional neutron network

--- Regconition_main.py
+++ Regconition_main.py
@@ -1,143 +1,129 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Mar 24 13:50:42 2020
+Created on Sat Mar 28 18:49:14 2020
 
 @author: Hister
 """
 
-# Random Forest Classification
+# Part 1 - Building the CNN
 
-# Importing the libraries
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+import time
+
+
+
+# Initialising the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution
+# 32, num feature detectors 3 by 3
+classifier.add(Convolution2D(20, 5, 5, input_shape = (128, 128, 3), activation = 'relu'))
+
+# Step 2 - Max Pooling
+# Usually we use 2 by 2
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Adding a second convolutional layer
+classifier.add(Convolution2D(40, 5, 5, activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+
+# Step 3 - Flattening
+classifier.add(Flatten())
+
+# Step 4 - Full Connection
+# Hidden layer
+classifier.add(Dense(output_dim = 40, activation = 'relu'))
+
+# Output layer
+classifier.add(Dense(output_dim = 3, activation = 'softmax'))
+
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+
+# Part 2 - Fitting the CNN to the images
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)# rescale the value to between 0 and 1
+
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                 target_size = (128, 128),
+                                                 batch_size = 3,
+                                                 class_mode = 'categorical')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size = (128, 128),
+                                            batch_size = 3,
+                                            class_mode = 'categorical')
+
+
+
+starttime = time.time()
+
+classifier.fit_generator(training_set,
+                         epochs = 100,
+                         validation_data = test_set,
+                         workers = 6)
+
+print('That took {} seconds'.format(time.time() - starttime))
+
+
+
+
+"""
 import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-from PIL import Image
-from pylab import array
-from pylab import *
+from keras.preprocessing import image
+
+y_pred = []
+for i in range(0,21):
+    test_image = image.load_img('dataset/folder_test/test_{}.png'.format(i), target_size = (128, 128))
+    test_image = image.img_to_array(test_image)
+    result = classifier.predict_classes(np.expand_dims(test_image, axis = 0))
+    y_pred.append(result)
+"""
 
 
-# Importing the dataset
-X_train = []
-y_train = []
-X_test = []
-y_test = []
+import numpy as np
+from keras.preprocessing import image
 
-
-pil_im = Image.open('dataset/folder_test/test_2.png').convert('LA').resize((128,128))
-imArray = array(pil_im).flatten()
-width, height = pil_im.size 
-
-left = width / 4
-top = height / 4
-right = width / 4
-bottom = height / 4
-
-im1 = pil_im.crop((left, top, right, bottom)) 
-im1.show()
-
-imshow(pil_im)
-show()
+y_pred = []
+for i in range(0,1):
+    test_image = image.load_img('dataset/folder_test2/test_{}.png'.format(i), target_size = (128, 128))
+    test_image = image.img_to_array(test_image)
+    result = classifier.predict_classes(np.expand_dims(test_image, axis = 0))
+    y_pred.append(result)
 
 
 
-# Import Image to train
-for _ in range(0, 1):
-    for i in range(1, 31):
-        pil_im = Image.open('dataset/folder_3/3_{}.png'.format(i)).convert('LA').resize((128,128))
-        imArray = array(pil_im).flatten()
-        X_train.append(imArray)
-        y_train.append(3)
-
-    for i in range(1, 31):
-        pil_im = Image.open('dataset/folder_4/4_{}.png'.format(i)).convert('LA').resize((128,128))
-        imArray = array(pil_im).flatten()
-        X_train.append(imArray)
-        y_train.append(4)
-    
-    for i in range(1, 26):
-        pil_im = Image.open('dataset/folder_5/5_{}.png'.format(i)).convert('LA').resize((128,128))
-        imArray = array(pil_im).flatten()
-        X_train.append(imArray)
-        y_train.append(5)
-
-# test set
-X_test = []
-for i in range(1, 5):
-    pil_im = Image.open('dataset/folder_test/test_{}.png'.format(i)).convert('LA').resize((128,128))
-    imArray = array(pil_im).flatten()
-    X_test.append(imArray)
 
 
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+"""
+# Using steps_per_epoch and validation_steps instand of samples per epoch
+starttime = time.time()
+classifier.fit_generator(training_set,
+                         steps_per_epoch = 250, 
+                         epochs = 25,
+                         validation_data = test_set,
+                         validation_steps = 63, 
+                         workers = 6)
+print('That took {} seconds'.format(time.time() - starttime))
+"""
 
-# Fitting Random Forest Classification to the Training set
-from sklearn.ensemble import RandomForestClassifier
-classifier = RandomForestClassifier(n_estimators = 100, criterion = 'entropy')
-classifier.fit(X_train, y_train)
 
-    
-
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
-
+"""
 # Making the Confusion Matrix
-#from sklearn.metrics import confusion_matrix
-#cm = confusion_matrix(y_test, y_pred)
-
-
-import cv2
-import matplotlib.pyplot as plt
-import cvlib as cv
-from cvlib.object_detection import draw_bbox
-im = cv2.imread('folder_test/test_2.png.jpg')
-bbox, label, conf = cv.detect_common_objects(im)
-output_image = draw_bbox(im, bbox, label, conf)
-plt.imshow(output_image)
-plt.show()
-
-
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
 """
 
-# Visualising the Training set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_train, y_train
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Random Forest Classification (Training set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
-
-
-
-# Visualising the Test set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_test, y_test
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Random Forest Classification (Test set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
-
-"""
